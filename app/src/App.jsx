3e932c708f2d05dc04ad6bc1fc89764bde073a3b
meghnaravikumar <meghnravikumar@gmail.com>
2024-07-30 18:06:48 -0400

    Prototype shown to Salma

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import Viewport from './components/Viewport';
+import Scene from './components/Scene';
+import ShapeSelector from './components/ShapeSelector';
+import Toolbar from './components/Toolbar';
+import { ShapeProvider } from './components/ShapeContext';
+import { CameraProvider } from './components/CameraContext';
+import AnimationEditor from './components/AnimationEditor';
 
 const App = () => {
   return (
-    <div className="app">
-      <Viewport />
-    </div>
+    <ShapeProvider>
+      <CameraProvider>
+        <div className="flex flex-col h-screen overflow-hidden">
+          <Toolbar />
+          <div className="flex flex-1 overflow-hidden">
+            <div className="flex-none w-48 bg-gray-200 flex flex-col items-center justify-center">
+              select some shapes!
+              <ShapeSelector />
+            </div>
+            <div className="flex-1 flex bg-white">
+              <Scene />
+            </div>
+            <div className="flex-none w-48 bg-gray-200 flex flex-col items-center justify-center">
+              <AnimationEditor />
+            </div>
+          </div>
+          <div className="flex-none h-24 bg-gray-800 text-white flex items-center justify-center">
+            Keyframe timeline goes here!
+          </div>
+        </div>
+      </CameraProvider>
+    </ShapeProvider>
   );
 };
 
