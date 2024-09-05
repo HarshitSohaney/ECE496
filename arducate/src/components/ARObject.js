990231a4b7b0b80cbac288c2f12a18a4b51904d3
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-09-05 15:22:50 -0400

    Add edges to objects
    commit-id:84fa341e

--- arducate/src/components/ARObject.js
+++ arducate/src/components/ARObject.js
@@ -3,6 +3,7 @@ import React, { forwardRef, useImperativeHandle, useRef } from "react";
 import { useAtom } from "jotai";
 import { selectedObjectAtom } from "../atoms";
 import { getAsset } from "./Assets"
+import { Edges } from "@react-three/drei";
 
 const ARObject = forwardRef(({ object, isSelected, setTransformControlsRef }, ref) => {
   const [, setSelectedObject] = useAtom(selectedObjectAtom);
@@ -18,18 +19,35 @@ const ARObject = forwardRef(({ object, isSelected, setTransformControlsRef }, re
     setTransformControlsRef(meshRef.current);
   };
 
+  /*
+    This code creates darker edges for the object, based on the object color:
+      1. It takes the object's color and converts it to a darker shade
+      2. The color is first converted from hex to RGB
+      3. Each RGB component is darkened by subtracting 20 (clamped to 0)
+      4. The darkened RGB is then converted back to hex
+  */
+  function getDarkerColor(color) {
+    return color.replace(/^#(..)(..)(..)$/, (_, r, g, b) => {
+      const darken = (c) => Math.max(0, parseInt(c, 16) - 20).toString(16).padStart(2, '0');
+      
+      // Return as hex string "#RRGGBB"
+      return `#${darken(r)}${darken(g)}${darken(b)}`;
+    });
+  }
+
   return (
-    <mesh
-      ref={meshRef}
-      position={object.position || [0, 0, 0]}
-      scale={object.scale || [1, 1, 1]}
-      rotation={object.rotation || [0, 0, 0]}
-      onPointerDown={handlePointerDown}
-    >
-      {/* Add any geometry you want, e.g., a box */}
-      {getAsset(object.type)}
-      <meshStandardMaterial color={object.color || "orange"} />
-    </mesh>
+      <mesh
+        ref={meshRef}
+        position={object.position || [0, 0, 0]}
+        scale={object.scale || [1, 1, 1]}
+        rotation={object.rotation || [0, 0, 0]}
+        onPointerDown={handlePointerDown}
+      >
+        {/* Add any geometry you want, e.g., a box */}
+        {getAsset(object.type)}
+        <meshStandardMaterial color={object.color || "orange"}/>
+        <Edges lineWidth={2} color={getDarkerColor(object.color)} />
+      </mesh>
   );
 });
 
