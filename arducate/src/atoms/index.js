66dfa22990fa2d72c1c895bde201901feaf42481
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-09-20 14:14:02 -0400

    Fix dev by adding back the missing atoms
    commit-id:c2d7ae28

--- arducate/src/atoms/index.js
+++ arducate/src/atoms/index.js
@@ -22,3 +22,9 @@ export const addAssetAtom = atom('');
 
 // Atom to manage the transform mode ('translate', 'rotate' or 'scale')
 export const transformModeAtom = atom('translate');
+
+// Atoms to manage the animation editor UI
+export const timelineScaleAtom = atom(100);
+export const currentTimeAtom = atom(0);
+export const isPlayingAtom = atom(false);
+export const timelineWidthAtom = atom(0);
