52d9448fafc4ab589387884b750c93efd4dfa52b
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-10-31 14:21:58 -0400

    Animation in VR works

--- arducate/src/components/ARPublish.js
+++ arducate/src/components/ARPublish.js
@@ -51,8 +51,6 @@ const convertSceneToVR = (arObjects) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://aframe.io/releases/1.2.0/aframe.min.js"></script>
-        <script src="https://aframe.io/extras/aframe-extras.min.js"></script>
-        <script src="https://unpkg.com/aframe-text-geometry-component@0.5.1/dist/aframe-text-geometry-component.min.js"></script>
       </head>
       <body>
         <a-scene>
--- arducate/src/components/Conversion/VRPublish.js
+++ arducate/src/components/Conversion/VRPublish.js
@@ -15,14 +15,16 @@ export const convertSceneToVR = (vrObjects) => {
       <title>WebVR Scene</title>
       <script src="https://aframe.io/releases/1.6.0/aframe.min.js"></script>
       <script src="https://unpkg.com/aframe-text-geometry-component@0.5.1/dist/aframe-text-geometry-component.min.js"></script>
-      <script src="https://aframe.io/extras/aframe-extras.min.js"></script>
       <script src="./globalAnimationCoordinator.js"></script>
     </head>
     <body style="margin: 0; overflow: hidden;">
-      <a-scene embedded>
+      <a-scene>
+        <a-entity position="0 1.5 0">
           ${vrObjects.map(object => renderObject(object)).join("")}
-          <a-sky color="#ECECEC"></a-sky>
-          <a-camera position="0 0 4" look-controls="enabled:true"></a-camera>
+        </a-entity>
+        
+        <!-- Camera with movement and look controls -->
+        <a-entity camera look-controls wasd-controls position="0 1 3"></a-entity>
       </a-scene>
     </body>
     </html>`;
