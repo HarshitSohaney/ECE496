9a6c55def4ea7c8b0bcd26573ae18adeb7fdd604
Harshit Sohaney <73911621+HarshitSohaney@users.noreply.github.com>
2024-11-08 16:58:48 -0500

    Fix issue with text not converting to ar vr anymore

--- arducate/src/components/Conversion/utils.js
+++ arducate/src/components/Conversion/utils.js
@@ -67,22 +67,58 @@ export const renderObject = (object) => {
   const initialPosition = getInitialPosition(object);
   const animations = generateAnimations(object.keyframes);
 
-  return `
-    <a-entity position="${initialPosition}" ${animations}>
-      <${object.entity}
-          scale="${object.scale.join(" ")}" 
-          rotation="${object.rotation.join(" ")}"
-          color="${object.color}">
-      </${object.entity}>
-      <a-text visible="${object.showLabel}" 
-          value="${object.name || `Object ${object.id}`}"
-          position="0 ${-object.scale[1]} 0"
-          render-order="1"
-          scale="0.5 0.5 0.5"
-          align="center"
-          color="#000000"
-          opacity="0.8"
-          side="double">
-      </a-text>
-    </a-entity>`;
+  const commonPosition = object.position.join(" ");
+  const commonScale = object.scale.join(" ");
+  const commonRotation = object.rotation.join(" ");
+  const commonTextLabel = renderTextLabel(object);
+  
+  switch (object.entity) {
+    case "a-text":
+      return `
+        <a-entity>
+          <${object.entity} 
+            value="${object.text}"
+            align="center"
+            anchor="center"
+            position="${commonPosition}"
+            scale="${commonScale}"
+            rotation="${commonRotation}"
+            color="#000000"></${object.entity}>
+          ${commonTextLabel}
+        </a-entity>
+      `;
+
+    case "a-element":
+      return `
+        <a-entity>
+          <a-entity 
+            line="color: black; lineWidth: 2; start: 0 0 0; end: 0 2 0" 
+            position="${commonPosition}"
+            scale="${commonScale}"
+            rotation="${commonRotation}">
+          </a-entity>
+          ${commonTextLabel}
+        </a-entity>
+      `;
+
+    default:
+      return `
+      <a-entity position="${initialPosition}" ${animations}>
+        <${object.entity}
+            scale="${object.scale.join(" ")}" 
+            rotation="${object.rotation.join(" ")}"
+            color="${object.color}">
+        </${object.entity}>
+        <a-text visible="${object.showLabel}" 
+            value="${object.name || `Object ${object.id}`}"
+            position="0 ${-object.scale[1]} 0"
+            render-order="1"
+            scale="0.5 0.5 0.5"
+            align="center"
+            color="#000000"
+            opacity="0.8"
+            side="double">
+        </a-text>
+      </a-entity>`;
+  }
 };
