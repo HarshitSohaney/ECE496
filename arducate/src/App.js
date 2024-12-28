46e5350a3522b98bc422971efdf7f99e81dbdf97
yunjia <yunjia.hao@mail.utoronto.ca>
2024-12-28 11:12:23 -0500

    add changes

--- arducate/src/App.js
+++ arducate/src/App.js
@@ -1,4 +1,4 @@
-// src/App.js.
+// src/App.js
 import React from 'react';
 import ARCanvas from './components/Canvas';
 import ARControls from './components/ARControls';
@@ -9,13 +9,16 @@ import SequenceEditor from 'components/SequenceEditor/SequenceEditor';
 const App = () => {
   return (
     <div className="flex flex-col items-center h-screen">
-      <Toolbar className="flex-shrink-0"/>
-      <div className="flex flex-1">
-        <Sidebar />
-        <ARCanvas />
-        <ARControls />
+      <div className="flex flex-1 flex-col">
+        <div className="flex flex-1">
+          <Sidebar />
+          <ARCanvas />
+          <ARControls />
+        </div>
+        <div className="flex-shrink-0">
+          <SequenceEditor />
+        </div>
       </div>
-      <SequenceEditor />
     </div>
   );
 };
